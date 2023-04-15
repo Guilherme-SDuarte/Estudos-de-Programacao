cca2209b171b2b4fbfcae8e03b22f6705a006130
Guilherme Duarte <guilhermeduarte1405@gmail.com>
2023-04-14 23:14:43 -0300

    hfd

--- python3/EXERCÍCIOS/teste01/desafio01.py
+++
@@ -1,3 +0,0 @@
-print ('==== Desafio 01 ====')
-nome = input('Qual o seu nome? ')
-print('Olá',nome,'Prazer em te conhecer!')
--- python3/EXERCÍCIOS/teste01/desafio03.py
+++
@@ -1,4 +0,0 @@
-print('==== Desafio 03 ====')
-num1 = input('Primeiro Número: ')
-num2 = input('Segundo Número: ')
-print('A soma é', num1 + num2)
