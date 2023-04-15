7a2114dc175d2946330afb4f5b69be475fa93fdd
Guilherme Duarte <guilhermeduarte1405@gmail.com>
2023-04-15 17:17:13 -0300

    python

--- python/desafios-pyhton/desafio003.py
+++ python/desafios-pyhton/desafio003.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 s = n1 + n2
-print('O resultado é: {}'.format(s))
+print('O resultado entre {} e {} é {}!'.format(n1, n2, s))
